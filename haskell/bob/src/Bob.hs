04335ad3a58c94aed5db3db74e0ab0632cafebba
Michael Wang <mwwpublic@gmail.com>
2016-12-28 16:37:39 -0500

    add bob exercise

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,14 @@
 module Bob (responseFor) where
 
+capCounter :: String -> Integer
+capCounter = foldl (\s x -> if x `elem` ['A'..'Z'] then s + 1 else s) 0
+
+hasLower :: String -> Bool
+hasLower v = foldl (\s x -> if x `elem` ['a'..'z'] then s + 1 else s) 0 v > 0
+
 responseFor :: String -> String
-responseFor = undefined
+responseFor x
+  | x == "Bob" || null (words x) = "Fine. Be that way!"
+  | any (>= 2) (map capCounter (words x)) && not (hasLower x) = "Whoa, chill out!"
+  | last (last (words x)) == '?' = "Sure."
+  | otherwise = "Whatever."
